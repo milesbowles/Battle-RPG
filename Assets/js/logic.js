ae64bf1d6a2b7eeeb6c45efb4695be60c5f8226e
Miles <Milesvbowles@gmail.com>
2017-09-23 12:46:06 -0700

    Various updates

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -6,22 +6,24 @@ $(document).ready(function() {
     audioElement.setAttribute("src", "Assets/captainplanet24.mp3");
 
     var gameViewWidth = $('#gameView').width();
-    var move_back = '-=' + (gameViewWidth / 20) + 'px';
-    var move_forward = '+=' + (gameViewWidth / 20) + 'px';
 
 
     var player = {
+        turn: true,
         power: 30,
         defence: 100,
         hitPoints: 100,
-        speed: 10,
+        speed: 20,
         weapon: 'none',
+        location: document.querySelector('#player'),
         moveForward: function () {
+            var move_forward = '+=' + (gameViewWidth / this.speed) + 'px';
             $('#player-img').attr('src', 'Assets/media/basic-tank/move-forward.gif');
             $('#player').animate({ left: move_forward }, 1800);
             this.stop();
         },
         moveBackward: function () {
+            var move_back = '-=' + (gameViewWidth / this.speed) + 'px';
             $('#player-img').attr('src', 'Assets/media/basic-tank/move-backward.gif');
             $('#player').animate({ left: move_back }, 1800);
             this.stop();
@@ -29,10 +31,25 @@ $(document).ready(function() {
         stop: function () {
             setTimeout(function() {
                 $('#player-img').attr('src', 'Assets/media/basic-tank/stationary.gif');
-            }, 1800)
+            }, 1800);
+            this.endTurn();
         },
         attack: function () {
-            
+            if (distanceBetweenElements(enemy.location,player.location) < 500){
+                console.log('distance func works!');
+                enemy.hitPoints = enemy.hitPoints - roll(player.power);
+                console.log(enemy.hitPoints);
+                enemy.updateHitPoints();
+            }
+            player.endTurn();
+        },
+        updateHitPoints: function () {
+            var percentage = this.hitPoints.toString() + '%';
+            $('#player-hit-points').attr('aria-valuenow', this.hitPoints).css('width', percentage).text(this.hitPoints.toString());
+        },
+        endTurn: function () {
+            $('#controlView').hide();
+            enemy.turn();
         }
     };
 
@@ -42,12 +59,15 @@ $(document).ready(function() {
         hitPoints: 100,
         speed: 10,
         weapon: 'none',
+        location: document.querySelector('#enemy'),
         moveForward: function () {
-            $('#player-img').attr('src', 'Assets/media/basic-tank/move-forward.gif');
-            $('#player').animate({ left: move_forward }, 1800);
+            var move_forward = '-=' + (gameViewWidth / this.speed) + 'px';
+            $('#enemy-img').attr('src', 'Assets/media/basic-tank/move-forward.gif');
+            $('#enemy').animate({ left: move_forward }, 1800);
             this.stop();
         },
         moveBackward: function () {
+            var move_back = '+=' + (gameViewWidth / this.speed) + 'px';
             $('#player-img').attr('src', 'Assets/media/basic-tank/move-backward.gif');
             $('#player').animate({ left: move_back }, 1800);
             this.stop();
@@ -55,20 +75,57 @@ $(document).ready(function() {
         stop: function () {
             setTimeout(function() {
                 $('#player-img').attr('src', 'Assets/media/basic-tank/stationary.gif');
-            }, 1800)
+            }, 1800);
+            this.endTurn();
         },
         attack: function () {
+            player.hitPoints = player.hitPoints - roll(enemy.power);
+            player.updateHitPoints();
+            this.endTurn();
+        },
+        block: function () {
+            this.endTurn();
+        },
+        updateHitPoints: function () {
+            var percentage = this.hitPoints.toString() + '%';
+            $('#enemy-hit-points').attr('aria-valuenow', this.hitPoints).css('width', percentage).text(this.hitPoints.toString());
+        },
+        turn: function () {
+            var randomNumber = Math.random();
+            setTimeout(function() {
+                if (distanceBetweenElements(enemy.location,player.location) < 500){
+                    if (randomNumber >= 0.3) {
+                        enemy.attack();
+                    } else {
+                        enemy.block();
+                    }
 
+                } else {
+                    enemy.moveForward();
+                }
+            }, 1800);
+        },
+        endTurn: function () {
+            $('#controlView').show()
         }
     };
 
+    function roll(max) {
+        return Math.floor(Math.random() * max) + 1
+    }
 
-// .delay(1800).attr('src', 'Assets/media/basic-tank/stationary.gif')
-
+    function distanceBetweenElements(element1, element2) {
+        var e1Rect = element1.getBoundingClientRect();
+        var e2Rect = element2.getBoundingClientRect();
+        var dx = (e1Rect.left+(e1Rect.right-e1Rect.left)/2) - (e2Rect.left+(e2Rect.right-e2Rect.left)/2);
+        var dy = (e1Rect.top+(e1Rect.bottom-e1Rect.top)/2) - (e2Rect.top+(e2Rect.bottom-e2Rect.top)/2);
+        var distance = Math.sqrt(dx * dx + dy * dy);
+        return distance;
+    }
 
     // Move Buttons
-    $(".up-button").on("click", function() {
-        $(".captain-planet").animate({ top: "-=200px" }, "normal");
+    $("#attack-btn").on("click", function() {
+        player.attack();
     });
 
     $(".down-button").on("click", function() {
@@ -87,35 +144,4 @@ $(document).ready(function() {
         $(".captain-planet").animate({ top: "50px", left: "80px" }, "fast");
     });
 
-    // Move Buttons (Keyboard Down)
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 40:
-                $(".captain-planet").animate({ top: "+=200px" }, "normal");
-        }
-    });
-
-    // Move Buttons (Keyboard Right)
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 39:
-                $(".captain-planet").animate({ left: "+=200px" }, "normal");
-        }
-    });
-
-    // Move Buttons (Keyboard Up)
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 38:
-                $(".captain-planet").animate({ top: "-=200px" }, "normal");
-        }
-    });
-
-    // Move Buttons (Keyboard Left)
-    $(document).keyup(function(e) {
-        switch (e.which) {
-            case 37:
-                $(".captain-planet").animate({ left: "-=200px" }, "normal");
-        }
-    });
 });
